21630793e138ae66de9d12d5622e9b54e41dbd91
johndoknjas <jdoknjas@sfu.ca>
2024-09-10 23:09:46 -0700

    Some cleanups in `compare.py`.

--- compare.py
+++ compare.py
@@ -24,6 +24,7 @@ def first7HandIsBetter(h1: list[int], h2: list[int]) -> bool | None:
 
 
 def getBestFrom7(sevenCards: list[int], sevenSuits: list[int]):
+    # todo - see if this can be sped up, why are the i-j loops iterating 42 times and not 21.
     # given 7 cards, call the 5-card comparator on each of the 21 poss. combos
     bestHand = None
     for i in range(7):
@@ -40,69 +41,41 @@ def getBestFrom7(sevenCards: list[int], sevenSuits: list[int]):
     assert bestHand
     return bestHand
 
-
 def getHandRankFromFiveCards(fC: list[int], fS: list[int]):
+    """`fC` are the five values and `fS` are the five suits"""
     # given 5 cards, determine what the rank of the hand is and add kicker info to it
     if fS.count(fS[0]) == len(fS):
-        (
-            fC.append(8)
-            if (
-                (fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3)
-                and (fC[4] - 1 == fC[3] or fC[4] - 12 == fC[0])
-            )
-            else fC.append(5)
-        )
-    elif (fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3) and (
-        fC[4] - 1 == fC[3] or fC[4] - 12 == fC[0]
-    ):
+        # flush, see if it's a regular flush or a straight flush
+        fC.append(8 if (    (fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3)
+                        and (fC[4] - 1 == fC[3] or fC[4] - 12 == fC[0]))
+                    else 5)
+    elif ((fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3) and
+          (fC[4] - 1 == fC[3] or fC[4] - 12 == fC[0])):
         fC.append(4)  # straight
     elif fC[1] == fC[2] == fC[3] and (fC[0] == fC[1] or fC[3] == fC[4]):
-        (
-            fC.extend([7, fC[0], fC[4]])
-            if fC[0] == fC[1]
-            else fC.extend([7, fC[4], fC[0]])
-        )  # quads
+        fC.extend((7, fC[0], fC[4]) if fC[0] == fC[1] else (7, fC[4], fC[0])) # quads
     elif fC[0] == fC[1] == fC[2] and fC[3] == fC[4]:
-        fC.extend([6, fC[0], fC[4]])  # boat, high set full of low pair
+        fC.extend((6, fC[0], fC[4]))  # boat, high set full of low pair
     elif fC[0] == fC[1] and fC[2] == fC[3] == fC[4]:
-        fC.extend([6, fC[4], fC[0]])  # boat, low set full of high pair
+        fC.extend((6, fC[4], fC[0]))  # boat, low set full of high pair
     elif fC[0] == fC[1] == fC[2]:
-        fC.extend(
-            [3, fC[0], fC[4], fC[3]]
-        )  # trips, both kickers higher; other kicker-types of trips in next line
+        fC.extend((3, fC[0], fC[4], fC[3]))
+        # trips, both kickers higher; other kicker-types of trips in next line
     elif fC[2] == fC[3] and (fC[1] == fC[2] or fC[3] == fC[4]):
-        (
-            fC.extend([3, fC[1], fC[4], fC[0]])
-            if fC[1] == fC[2]
-            else fC.extend([3, fC[2], fC[1], fC[0]])
-        )
-    elif (fC[0] == fC[1] and (fC[2] == fC[3] or fC[3] == fC[4])) or (
-        fC[1] == fC[2] and fC[3] == fC[4]
-    ):  # two pair
+            fC.extend((3, fC[1], fC[4], fC[0]) if fC[1] == fC[2] else (3, fC[2], fC[1], fC[0]))
+    elif ((fC[0] == fC[1] and (fC[2] == fC[3] or fC[3] == fC[4])) or
+          (fC[1] == fC[2] and fC[3] == fC[4])):  # two pair
         if fC[0] == fC[1] and fC[2] == fC[3]:
-            fC.extend([2, fC[3], fC[1], fC[4]])  # kicker higher than both pairs
+            fC.extend((2, fC[3], fC[1], fC[4]))  # kicker higher than both pairs
         else:
-            (
-                fC.extend([2, fC[4], fC[1], fC[2]])
-                if fC[0] == fC[1] and fC[3] == fC[4]
-                else fC.extend([2, fC[4], fC[1], fC[0]])
-            )
+            fC.extend((2, fC[4], fC[1], fC[2 if fC[0] == fC[1] and fC[3] == fC[4] else 0]))
     elif fC[0] == fC[1] or fC[1] == fC[2]:
-        (
-            fC.extend([1, fC[0], fC[4], fC[3], fC[2]])
-            if fC[0] == fC[1]
-            else fC.extend([1, fC[1], fC[4], fC[3], fC[0]])
-        )
+            fC.extend((1, fC[0], fC[4], fC[3], fC[2]) if fC[0] == fC[1] else
+                      (1, fC[1], fC[4], fC[3], fC[0]))
     elif fC[2] == fC[3] or fC[3] == fC[4]:
-        (
-            fC.extend([1, fC[2], fC[4], fC[1], fC[0]])
-            if fC[2] == fC[3]
-            else fC.extend([1, fC[3], fC[2], fC[1], fC[0]])
-        )
-    return (
-        fC if len(fC) > 5 else fC + [0]
-    )  # return hand, but first if we haven't appended anything else note that it's just a high card hand
-
+        fC.extend((1, fC[2], fC[4], fC[1], fC[0]) if fC[2] == fC[3] else (1, fC[3], fC[2], fC[1], fC[0]))
+    return fC if len(fC) > 5 else fC + [0]
+    # return hand, but first if we haven't appended anything else note that it's just a high card hand
 
 def is_first_hand_better(cards: str):
     """`cards` should be in a format like this: `KhQh AsJs 5c6dTh4dJd`"""
