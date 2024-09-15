77fd03f030ea7fa923400b5e1a3b441572424d5a
johndoknjas <jdoknjas@sfu.ca>
2024-09-15 02:22:58 -0700

    Some more cleanups.

--- compare.py
+++ compare.py
@@ -14,6 +14,7 @@ class GameType(Enum):
 
 _ranks = {}
 _num_card_vals: Optional[int] = None
+_mapping = str.maketrans("TJQKA", ":;<=>")
 
 def set_gametype(gametype: GameType):
     global _ranks, _num_card_vals
@@ -35,21 +36,15 @@ def num_card_vals() -> int:
 def first7HandIsBetter(h1: list[int], h2: list[int]) -> bool | None:
     # given two hands, with the 5 cards + rank + relevant kicker details, say which wins
     if h1[5] != h2[5]:
-        return h1[5] > h2[5]  # different ranks
-    if h1[5] == 8 or h1[5] == rank('straight'):
+        return h1[5] > h2[5] # different ranks
+    if h1[5] in (rank('straight'), 8):
         # SF or straight: check middle card, and if needed check if an ace is actually the low card
         return h1[2] > h2[2] if h1[2] != h2[2] else h1[4] < h2[4] if h1[4] != h2[4] else None
-    if h1[5] == rank('flush') or h1[5] == 0:  # flush or high card: check all five cards
-        for wooper in range(4, -1, -1):
-            if h1[wooper] != h2[wooper]:
-                return h1[wooper] > h2[wooper]
-        return None  # chop
-    # The hands must both be one of quads, trips, boat, two pair, or one pair.
+    if h1[5] in (0, rank('flush')): # flush or high card: check all five cards
+        return next((h1[i] > h2[i] for i in (4,3,2,1,0) if h1[i] != h2[i]), None)
+    # The hands must both be one of quads, trips, boat, two pair, or one pair:
     assert 8 <= len(h1) == len(h2) <= 10
-    for scromp in range(6, len(h1)):
-        if h1[scromp] != h2[scromp]:
-            return h1[scromp] > h2[scromp]  # one is higher, so that one wins
-    return None
+    return next((h1[i] > h2[i] for i in range(6, len(h1)) if h1[i] != h2[i]), None)
 
 def getBestFrom7(sevenCards: tuple[int, ...], sevenSuits: tuple[int, ...]) -> list[int]:
     """Given 7 cards, call the 5-card comparator on each of the 21 possible combos."""
@@ -68,48 +63,41 @@ def getHandRankFromFiveCards(fC: list[int], all_same_suit: bool):
     # given 5 cards, determine what the rank of the hand is and add kicker info to it
     if all_same_suit:
         # flush, see if it's a regular flush or a straight flush
-        fC.append(8 if (    (fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3)
+        fC.append(8 if (    all(fC[0] == fC[i] - i for i in (1,2,3))
                         and (fC[4] - 1 == fC[3] or fC[4] - (num_card_vals()-1) == fC[0]))
                     else rank('flush'))
-    elif ((fC[0] == fC[1] - 1 == fC[2] - 2 == fC[3] - 3) and
+    elif (all(fC[0] == fC[i] - i for i in (1,2,3)) and
           (fC[4] - 1 == fC[3] or fC[4] - (num_card_vals()-1) == fC[0])):
         fC.append(rank('straight'))  # straight
-    elif fC[1] == fC[2] == fC[3] and (fC[0] == fC[1] or fC[3] == fC[4]):
+    elif fC[1] == fC[3] in (fC[0], fC[4]):
         fC.extend((7, fC[0], fC[4]) if fC[0] == fC[1] else (7, fC[4], fC[0])) # quads
-    elif fC[0] == fC[1] == fC[2] and fC[3] == fC[4]:
+    elif fC[0] == fC[2] and fC[3] == fC[4]:
         fC.extend((rank('boat'), fC[0], fC[4]))  # boat, high set full of low pair
-    elif fC[0] == fC[1] and fC[2] == fC[3] == fC[4]:
+    elif fC[0] == fC[1] and fC[2] == fC[4]:
         fC.extend((rank('boat'), fC[4], fC[0]))  # boat, low set full of high pair
-    elif fC[0] == fC[1] == fC[2]:
+    elif fC[0] == fC[2]:
         fC.extend((rank('trips'), fC[0], fC[4], fC[3]))
         # trips, both kickers higher; other kicker-types of trips in next line
-    elif fC[2] == fC[3] and (fC[1] == fC[2] or fC[3] == fC[4]):
+    elif fC[2] == fC[3] in (fC[1], fC[4]):
         fC.append(rank('trips'))
         fC.extend((fC[1], fC[4], fC[0]) if fC[1] == fC[2] else (fC[2], fC[1], fC[0]))
-    elif ((fC[0] == fC[1] and (fC[2] == fC[3] or fC[3] == fC[4])) or
+    elif ((fC[0] == fC[1] and fC[3] in (fC[2], fC[4])) or
           (fC[1] == fC[2] and fC[3] == fC[4])):  # two pair
         if fC[0] == fC[1] and fC[2] == fC[3]:
             fC.extend((2, fC[3], fC[1], fC[4]))  # kicker higher than both pairs
         else:
             fC.extend((2, fC[4], fC[1], fC[2 if fC[0] == fC[1] and fC[3] == fC[4] else 0]))
-    elif fC[0] == fC[1] or fC[1] == fC[2]:
+    elif fC[1] in (fC[0], fC[2]):
         fC.extend((1, fC[0], fC[4], fC[3], fC[2]) if fC[0] == fC[1] else (1, fC[1], fC[4], fC[3], fC[0]))
-    elif fC[2] == fC[3] or fC[3] == fC[4]:
+    elif fC[3] in (fC[2], fC[4]):
         fC.extend((1, fC[2], fC[4], fC[1], fC[0]) if fC[2] == fC[3] else (1, fC[3], fC[2], fC[1], fC[0]))
     # If we haven't appended anything, note that it's a highcard hand by appending a zero:
     return fC if len(fC) > 5 else fC + [0]
 
 def is_first_hand_better(cards: str):
     """`cards` should be in a format like this: `KhQh AsJs 5c6dTh4dJd`"""
-    cards = (cards.replace("T", ":").replace("J", ";").replace("Q", "<")
-                  .replace("K", "=").replace("A", ">"))
-    c = [ord(card) for card in cards]
-    h1 = getBestFrom7(
-        (c[0], c[2], c[10], c[12], c[14], c[16], c[18]),
-        (c[1], c[3], c[11], c[13], c[15], c[17], c[19]),
-    )
-    h2 = getBestFrom7(
-        (c[5], c[7], c[10], c[12], c[14], c[16], c[18]),
-        (c[6], c[8], c[11], c[13], c[15], c[17], c[19])
-    )
-    return first7HandIsBetter(h1, h2)
+    groups = tuple(tuple(ord(card) for card in section.translate(_mapping)) for section in cards.split())
+    assert len(groups) == 3 and all(len(group) % 2 == 0 for group in groups)
+    comm_vals, comm_suits = groups[2][::2], groups[2][1::2]
+    return first7HandIsBetter(getBestFrom7(groups[0][::2] + comm_vals, groups[0][1::2] + comm_suits),
+                              getBestFrom7(groups[1][::2] + comm_vals, groups[1][1::2] + comm_suits))
