97392ba2a576d16d94fdfdae98ea61a90ba34e39
johndoknjas <jdoknjas@sfu.ca>
2024-09-17 04:57:15 -0700

    Add a new `discrepancy.py` script that compares two sim results files and outputs discrepancies (useful for testing). Also update `combiner.py` to re-rank hands (useful if different processes had been run for different chunks of hand types).

--- combiner.py
+++ combiner.py
@@ -4,6 +4,15 @@ import os
 def sort_key(line: str) -> str:
     return ' '.join(sub for sub in line.split() if not sub.endswith(('%', ':')))
 
+def percentage_val(line: str) -> float:
+    return next(float(word[:-1]) for word in line.split() if word.endswith('%'))
+
+def assign_rank(lines: list[str], line: str) -> int:
+    assert all(l.endswith(' opps') for l in lines)
+    similar_lines = [l for l in lines if l.split()[-2] == line.split()[-2]]
+    similar_lines.sort(key=lambda l: percentage_val(l), reverse=True)
+    return similar_lines.index(line) + 1
+
 def main() -> None:
     folder_name = sys.argv[1]
     lines: list[str] = []
@@ -15,6 +24,8 @@ def main() -> None:
         with open(f"{folder_name}/{filename}", 'r') as f:
             lines.extend(line.rstrip() for line in f)
     lines.sort(key=sort_key)
+    lines = [' '.join(line.split()[1:]) for line in lines]
+    lines = [f"#{assign_rank(lines, line)}: {line}" for line in lines]
     with open(f"{folder_name}/{output_filename}", 'w') as f:
         for line in lines:
             f.write(line + '\n')
---
+++ discrepancy.py
@@ -0,0 +1,38 @@
+"""Given two sim results files, output info about discrepancies between lines."""
+
+from __future__ import annotations
+import sys
+
+def readlines(file: str) -> list[str]:
+    with open(file, 'r') as f:
+        return [line.strip() for line in f]
+
+def get_components(line: str) -> tuple[str, float]:
+    equity_percentage = next(float(x[:-1]) for x in line.split() if x.endswith('%'))
+    text = ' '.join(x for x in line.split() if '#' not in x and '%' not in x)
+    return (text, equity_percentage)
+
+def main():
+    shorter, longer = sorted((readlines(sys.argv[1]), readlines(sys.argv[2])), key=len)
+    missing_lines = []
+    equity_percent_diffs: list[tuple[str, float]] = []
+    for line in longer:
+        components = get_components(line)
+        other = next((t for x in shorter if (t := get_components(x))[0] == components[0]), None)
+        if other is None:
+            missing_lines.append(components)
+            continue
+        equity_percent_diffs.append((components[0], abs(components[1] - other[1])))
+    equity_percent_diffs.sort(key=lambda d: d[1])
+    print("\nChanges in equity percentage in sorted order:")
+    for diff in equity_percent_diffs:
+        print(f"{diff[0].split()[0]} has a change in equity percentage of {diff[1]}%")
+    print(f"\nNum missing lines in the longer file but not the shorter: {len(missing_lines)}")
+    for line in missing_lines:
+        print(line)
+    print(f'Average change in equity percentage: ' +
+          f'{sum(j for _, j in equity_percent_diffs) / len(equity_percent_diffs)}%')
+
+
+if __name__ == '__main__':
+    main()
